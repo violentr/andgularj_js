1b9fa21c635e3a2b2a73ab4e8ae7fe4b145f512f
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-24 18:47:45 +0000

    Add Show green/red when user checkedIn or not
    introduce ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,18 @@ interface Passenger {
        <h3> Airline Passengers </h3>
        <ul>
          <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+            [class.checked-in]="passenger.checkedIn"
+          ></span>
+          {{ i }} : {{ passenger.fullname }}
+         </li>
+       </ul>
+       <h3> Airline Passengers </h3>
+       <ul>
+         <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+            [ngClass]="{'checked-in': passenger.checkedIn, 'checked-out': !passenger.checkedIn}"
+          ></span>
           {{ i }} : {{ passenger.fullname }}
          </li>
        </ul>
