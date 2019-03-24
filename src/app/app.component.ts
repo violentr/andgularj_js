ca36479c9ef5e8465bae9ea44ac2c6453d2038b6
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-24 15:58:00 +0000

    Add Bind variable to values
    interpolation, what happend
    behind the scenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:  `<div class="app"> {{ title }} </div>`,
+  template:  `
+     <div class="app">
+       <h1 [innerHTML]="title"> </h1>
+       <h1> {{ title }} </h1>
+       <div> {{ numberOne + numberTwo }} </div>
+     </div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+
   constructor(){
     this.title = "Ultimate Angular";
   }
