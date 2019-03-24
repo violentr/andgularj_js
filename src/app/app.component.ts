78dc3d16c6f0d2cd830618f5e1a23063ea63fca0
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-24 18:29:30 +0000

    Add Introduce ngFor directive, loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,56 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   template:  `
      <div class="app">
        <h1> {{ title }} </h1>
-       <img [src]="logo">
-       <input type="text"
-        [value]="name"
-        (input)="handleChange($event.target.value)">
-       <div *ngIf="name.length > 2"> Searching for... {{ name }} </div>
+       <h3> Airline Passengers </h3>
+       <ul>
+         <li *ngFor="let passenger of passengers; let i = index;">
+          {{ i }} : {{ passenger.fullname }}
+         </li>
+       </ul>
      </div>`,
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-
   title: string;
-  logo: string = "assets/img/logo.png";
-  name: string = "";
 
-  handleChange(value: string){
-    this.name = value
-  }
+  passengers: Passenger[] =[
+    {
+      id: 1,
+      fullname: 'Bill',
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: 'James',
+      checkedIn: false
+    },
+    {
+      id: 3,
+      fullname: 'Zoe',
+      checkedIn: true
+    },
+    {
+      id: 4,
+      fullname: 'Cassandra',
+      checkedIn: false
+    },
+    {
+      id: 5,
+      fullname: 'Julie',
+      checkedIn: true
+    },
+  ]
 
   constructor(){
     this.title = "Ultimate Angular";
