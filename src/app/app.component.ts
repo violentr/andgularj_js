af53fd556b89e97803775c787217888bd3f85443
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-24 19:03:58 +0000

    Add Option green/red with ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ interface Passenger {
        <ul>
          <li *ngFor="let passenger of passengers; let i = index;">
           <span class="status"
-            [style.backgroundColor]="(passenger.checkedIn ? 'green' : 'red' )"
+            [ngStyle]="{ 'backgroundColor': passenger.checkedIn ? 'green' : 'red' }"
           ></span>
           {{ i }} : {{ passenger.fullname }}
          </li>
