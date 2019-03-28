a4918f715b1ec21af07252e98dacf98453c44b37
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-28 21:06:45 +0000

    Move passenger info to PassengerDetail component

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface'
 @Component({
   selector: 'passenger-detail',
+  styleUrls: ['passenger-detail.component.scss'],
   template: `
    <div>
-     Detail component
+     <li>
+      <span class="status" [class.checked-in]="detail.checkedIn"></span>
+      {{ detail.fullname }}
+     <div class="date">Check in date: {{ detail.checkedIn ? (detail.checkedInDate | date: 'y MMMM d' | uppercase) :  'Not checked in'}} </div>
+         <div class="children"> Children: {{ detail.children?.length || 0}}</div>
+     </li>
+     <br />
    </div>`,
 
 })
 
 export class PassengerDetailComponent{
+
+  @Input()
+  detail: Passenger;
   constructor(){
   }
 }
--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -10,19 +10,10 @@ import { Passenger } from '../../models/passenger.interface'
        <passenger-count
          [items]="passengers">
        </passenger-count>
-       <passenger-detail></passenger-detail>
-       <h3> Airline Passengers </h3>
-       <ul>
-         <li *ngFor="let passenger of passengers; let i = index;">
-          <span class="status"
-            [class.checked-in]="passenger.checkedIn"
-          ></span>
-          {{ i }} : {{ passenger.fullname }}
-         <div class="date">Check in date: {{ passenger.checkedIn ? (passenger.checkedInDate | date: 'y MMMM d' | uppercase) :  'Not checked in'}} </div>
-         <br />
-         <div class="children"> Children: {{ passenger.children?.length || 0}}</div>
-         </li>
-       </ul>
+       <passenger-detail
+          *ngFor="let passenger of passengers"
+          [detail]="passenger">
+       </passenger-detail>
      </div>`,
 })
 
